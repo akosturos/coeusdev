628582d0747d453cb6f9cf5bf4d573070c043f80
Tony Kosturos <kosturosanthony@gmail.com>
2017-08-29 19:08:25 -0700

    changed title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
     #Returns the full title on a per-page basis
     def full_title(page_title = '')
-        base_title = "Coeus Development"
+        base_title = "Coeus"
         if page_title.empty?
             base_title
         else
