982839c7d0a91522cdc84da976d4323434813b89
Anthony Kosturos <kosturosanthony@gmail.com>
2017-09-06 11:43:26 -0700

    Revert "Changes database.yml config file to use postgres instead of mysql"

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170616040146) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -23,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170616040146) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
